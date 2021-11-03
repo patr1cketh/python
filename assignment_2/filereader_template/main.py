93899d771ed9f167d3015d9be1371691df7ed850
Tom Forde <ptrcketh@gmail.com>
2021-11-03 17:44:50 +0000

    almost done

--- assignment_2/filereader_template/main.py
+++ assignment_2/filereader_template/main.py
@@ -1,6 +1,88 @@
-from filereader import QuestionFileReader
+from filereader import FileReader, QuestionFileReader
+import random
 
-qfr_1 = QuestionFileReader()
+def control_flow():
+    response = ""
+    while response != "y" or "n":
+        response = input("Continue? (y/n) \n")
+        if response == "y":
+            break
+        elif response == "n":
+            print("Goodbye.")
+            quit()
+
+print("Creating an instance of the QuestionFileReader class...")
+qfr_1 = QuestionFileReader("question_bank_2.txt")
+print("-----\n")
+control_flow()
+
+print("Testing __str__() method...")
+print(qfr_1)
+print("-----\n")
+control_flow()
+
+"""
+print("Testing get_filename() method...")
+print("Filename: ", qfr_1.get_filename())
+print("-----\n")
+control_flow()
+
+print("Testing set_filename() method...")
+print("Setting filename to 'test.txt'...")
+qfr_1.set_filename("test.txt")
+print("Filename: ", qfr_1.get_filename())
+print("-----\n")
+control_flow()
+
+print("Testing filename property...")
+print("Setting filename back to 'question_bank_2.txt'...")
+qfr_1.filename("question_bank_2.txt")
+print("Getting filename....")
+print("Filename: ", qfr_1.filename)
+print("-----\n")
+control_flow()
+"""
+
+print("Testing get_line_amount() method...")
+print("Total number of lines in file: ", qfr_1.get_line_amount())
+print("-----\n")
+control_flow()
+
+print("Testing read_file() method...")
+print("Reading the entire contents of the file excluding line 0...")
+print(qfr_1.read_file())
+print("-----\n")
+control_flow()
+
+print("Testing read_random_range() method...")
+print("Reading a random range of lines...")
+print(qfr_1.read_random_range())
+print("-----\n")
+control_flow()
+
+print("Testing get_lines_at() method...")
+random_list = random.sample(range(1, qfr_1.get_line_amount()), random.randrange(1, qfr_1.get_line_amount()))
+print("Randomly generated list: ", random_list)
+print("Passing list into get_lines_at()...")
+print(qfr_1.get_lines_at(random_list))
+print("-----\n")
+control_flow()
+
+print("Testing all_dictionary_questions() method...")
+print("Getting all questions in the file in dictionary format...")
+print(qfr_1.all_dictionary_questions())
+print("-----\n")
+control_flow()
+
+print("Testing random_dictionary_questions() method...")
+print("Getting a random range of questions in dictionary format...")
+print(qfr_1.random_dictionary_questions())
+print("-----\n")
+control_flow()
+
+print("Testing complete.")
+print("Goodbye.")
+quit()
 
 
     
